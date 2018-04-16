a04c1dabac8a6f4ed0d1a06066f6c63ea521007b
imconfig <biluochun@users.noreply.github.com>
2018-04-16 15:35:37 +0800

    Update prepareProxy.js

--- packages/@vue/cli-service/lib/util/prepareProxy.js
+++ packages/@vue/cli-service/lib/util/prepareProxy.js
@@ -111,7 +111,8 @@ module.exports = function prepareProxy (proxy, appPublicFolder) {
   }
 
   // Otherwise, proxy is an object so create an array of proxies to pass to webpackDevServer
-  return Object.keys(proxy).map(context => {
+  const proxies = {}
+  Object.keys(proxy).forEach(context => {
     if (!proxy[context].hasOwnProperty('target')) {
       console.log(
         chalk.red(
@@ -122,8 +123,12 @@ module.exports = function prepareProxy (proxy, appPublicFolder) {
       process.exit(1)
     }
     const entry = createProxyEntry(proxy[context].target, proxy[context].onProxyReq, context)
-    return Object.assign({}, defaultConfig, proxy[context], entry)
+    Object.assign(proxies, {
+      [context]: Object.assign({}, defaultConfig, proxy[context], entry)
+    })
   })
+
+  return proxies
 }
 
 function resolveLoopback (proxy) {
